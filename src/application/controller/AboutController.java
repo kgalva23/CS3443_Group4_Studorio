225b1f144004c004a457b7c970f029132452abd9
connorSorenson <100450976+connorSorenson@users.noreply.github.com>
2022-04-23 17:02:26 -0500

    Update AboutController.java

--- src/application/controller/AboutController.java
+++ src/application/controller/AboutController.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.IOException;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -26,6 +27,36 @@ public class AboutController {
     	Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
     	window.setScene(scene);
     	window.show();
+    	
+    	/*		fixme comment above
+    	
+    		URL url = new File("src/application/view/MainMenu.fxml").toURI().toURL(); 
+		AnchorPane root = (AnchorPane)FXMLLoader.load(url);
+    	Scene scene = new Scene(root,400,400);
+    	Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+    	window.setScene(scene);
+    	
+    	
+    	*/
+    
     }
 
 }
+
+//import java.net.URL;		fixme
+/*add something similar to this in main
+
+@FXML
+private Button aboutButton;
+
+void handleAbout(ActionEvent event) throws IOException {
+URL url = new File("src/application/view/About.fxml").toURI().toURL(); 
+AnchorPane root = (AnchorPane)FXMLLoader.load(url);
+Scene scene = new Scene(root,400,400);
+Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+window.setScene(scene);
+
+}
+
+
+*/
